f05072e78991c5bd3ef2ce2111f006e9e8b950fd
Nazarii Lesniak <naziksay@gmail.com>
2026-01-10 12:36:20 +0200

    added row selection and sorting column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,65 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+
+tbody.addEventListener('click', (rowSelection) => {
+  rowSelection.preventDefault();
+
+  const clickedRow = rowSelection.target.closest('tr');
+  const activeRow = document.querySelector('.active');
+
+  if (activeRow !== null) {
+    activeRow.classList.remove('active');
+  }
+
+  if (!clickedRow) {
+    return;
+  }
+
+  clickedRow.classList.add('active');
+});
+
+let activeColumnTitle = 0;
+let isAscending = false;
+
+function getSalaryValue(salaryString) {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+}
+
+thead.addEventListener('click', (sortingColumn) => {
+  sortingColumn.preventDefault();
+
+  const rowsArrayData = Array.from(tbody.querySelectorAll('tr'));
+  const title = sortingColumn.target.closest('th');
+  const index = title.cellIndex;
+
+  if (!title) {
+    return;
+  }
+
+  if (index === activeColumnTitle) {
+    activeColumnTitle = index;
+    isAscending = !isAscending;
+  } else {
+    activeColumnTitle = index;
+    isAscending = true;
+  }
+
+  rowsArrayData.sort((firstRow, secondRow) => {
+    const firstValue = firstRow.cells[index].textContent;
+    const secondValue = secondRow.cells[index].textContent;
+
+    let result = 0;
+
+    if (index === 3 || index === 4) {
+      result = getSalaryValue(firstValue) - getSalaryValue(secondValue);
+    } else {
+      result = firstValue.localeCompare(secondValue);
+    }
+
+    return isAscending ? result : -result;
+  });
+
+  tbody.append(...rowsArrayData);
+});
